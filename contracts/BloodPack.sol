f0fb0c7eee6aa17c4b2b6a9e0e84ee04caf12e8c
Nguyen Phuc Thien An <npta97@gmail.com>
2019-07-25 11:00:39 +0700

    Update BloodPack contract

--- contracts/BloodPack.sol
+++ contracts/BloodPack.sol
@@ -6,7 +6,14 @@ import "./openzeppelin/math/SafeMath.sol";
 contract BloodPack {
     using SafeMath for uint256;
 
+    enum TransferType {
+        TransferBloodPack,
+        TransferBloodProduct,
+        ConsumeBloodProduct
+    }
+
     struct History {
+        TransferType transferType;
         string from;
         string to;
         string description;
@@ -34,8 +41,12 @@ contract BloodPack {
         createdAt = now.mul(1000);
     }
 
-    function transfer(string memory from, string memory to, string memory description) public onlyOwner {
-        History memory history = History(from, to, description, now.mul(1000));
+    function transfer(
+        TransferType transferType,
+        string memory from, string memory to,
+        string memory description
+    ) public onlyOwner {
+        History memory history = History(transferType, from, to, description, now.mul(1000));
         histories.push(history);
     }
 
@@ -43,9 +54,13 @@ contract BloodPack {
         return histories.length;
     }
 
-    function getHistory(uint index) public view returns (string memory, string memory, string memory, uint) {
+    function getHistory(uint index) public view returns (TransferType, string memory, string memory, string memory, uint) {
         require(index >= 0 && index < histories.length, "Index out of bounds");
         History memory history = histories[index];
-        return (history.from, history.to, history.description, history.transferedAt);
+        return (
+            history.transferType,
+            history.from, history.to,
+            history.description, history.transferedAt
+        );
     }
 }
